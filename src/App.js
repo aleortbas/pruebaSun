b62347cc3a0d4eb5421fbfae21ddea0466886286
Alejandro Ortiz <aleortbas@gmail.com>
2022-06-27 23:28:28 -0500

    refres test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 
+function refreshPage() {
+  window.location.reload(false);
+}
+
 class App extends Component {
 
 render(){
   return (
       <div>
-        <Main />
+        <Main {...refreshPage}/>
       </div>
   );
 }
